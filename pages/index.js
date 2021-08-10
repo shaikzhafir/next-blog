1a6ab880147c3e896bfb4eacab3c29016eb175bb
bobross96 <zadus96@gmail.com>
2021-08-11 00:45:29 +0800

    updated currently reading

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to the Bolg <Twemoji emoji="😀" />
         </h1>
-        <h3>Currently rereading: Slaughterhouse-Five by Kurt Vonnegut</h3>
+        <h3>Currently reading: Stoner by John Williams</h3>
         <br />
         <br />
 
