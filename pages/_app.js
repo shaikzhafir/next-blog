244ce616dbd2731d43a7171f8bdab21b427e6388
shaikzhafir <shaikzhafir@gmail.com>
2022-02-02 15:48:56 +0800

    loading thing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import router, { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import Spinner from "components/Spinner";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [pageLoading, setPageLoading] = useState(false);
+  useEffect(() => {
+    const handleStart = () => {
+      setPageLoading(true);
+    };
+    const handleComplete = () => {
+      setPageLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
+  return pageLoading ? (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Spinner />
+    </div>
+  ) : (
+    <Component {...pageProps} />
+  );
 }
 
-export default MyApp
+export default MyApp;
